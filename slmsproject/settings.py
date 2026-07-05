1fc3866c3aca53831bb1df2bff9cb6d619ecf3c3
Adityagodini <adityagodini@gmail.com>
2026-07-05 20:17:42 +0530

    added

--- slmsproject/settings.py
+++ slmsproject/settings.py
@@ -152,15 +152,17 @@ USE_TZ = True
 # =========================================================
 # STATIC FILES
 # =========================================================
-
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
